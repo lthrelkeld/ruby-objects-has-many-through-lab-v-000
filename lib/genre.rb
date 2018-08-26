f74562467f9eed8279c7811c32df48067cbe2764
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 18:13:03 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,21 @@
 class Genre
-  @@all = []
+  attr_accessor :name, :artist
 
-  def initialize()
+  @@all = []
+  @songs = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
   end
 
-  def self.all 
+  def self.all
     @@all
+  end
+
+  def new_song(name,artist)
+    song = Song.new(name,artist,self)
+    @songs << song
   end
 end
