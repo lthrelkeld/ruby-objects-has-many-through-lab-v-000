3a91b73ef90a08bdd12130aeea451efec3c8ce7a
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 17:46:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,10 +5,16 @@ class Artist
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def new_song(name,genre)
+    song = Song.new(name,artist,genre)
+    
+  end
+
 end
